5820f468a63a4e93e6f1f32fd1754f5c3e1348a5
DR-DOH <77018699+DR-DOH@users.noreply.github.com>
2021-01-10 20:16:38 +0530

    Update post_trial5.m

--- Scripts Updated/post_trial5.m
+++ Scripts Updated/post_trial5.m
@@ -1,5 +1,18 @@
 function post_trial5(path_main,state_path,fs,fs_new,rat,sd,channels,tetrodes)
 
+%input:-
+%path_main:- The path which contains recordings of all PT5 in a folder
+%state_path:- The path that contains the sleep scoring file of PT5
+%fs:- original sampling freq
+%fs_neww:- downsampled freq
+%rat:- rat number
+%sd:- study day number
+%channels:- cell array with each element being an array of channels of each tetrode
+%tetrodes:- cell array with tetrode numbers
+
+%output:-
+%outer function to loop around in the main script so that it can save the ripple detections for post trial 5, split into 4 sleep of 45mins eaach
+
 sleep_names = {'PT5.1','PT5.2','PT5.3','PT5.4'};
 filename = getstates(state_path,{'post_Trial5','post_trial5'});
 states = load(strcat(state_path,filename),'states');
