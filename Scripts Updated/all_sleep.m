08ce235149eba005a521e52429017c83220a7107
DR-DOH <77018699+DR-DOH@users.noreply.github.com>
2021-01-10 20:15:30 +0530

    Update all_sleep.m

--- Scripts Updated/all_sleep.m
+++ Scripts Updated/all_sleep.m
@@ -10,6 +10,9 @@ function all_sleep(path_main,state_path,fs,fs_new,rat,sd,channels,tetrodes)
 %channels:- cell array with each element being an array of channels of each tetrode
 %tetrodes:- cell array with tetrode numbers
 
+%output:-
+%outer function to loop around in the main script so that it can save the ripple detections
+
 sleep = {{'Presleep','presleep'} , {'post_Trial1','post_trial1'} , {'post_Trial2','post_trial2'} , {'post_Trial3','post_trial3'} , {'post_Trial4','post_trial4'}} ;
 sleep_names = {'PRE','PT1','PT2','PT3','PT4'};
 
