09dfd00f009a7416c15284478e9e1c256395812d
Adrián Alemán-Z <r.a.aleman.zapata@student.tue.nl>
2021-01-14 00:11:44 +0100

    Update all_tetrodes.m

--- Scripts Updated/all_tetrodes.m
+++ Scripts Updated/all_tetrodes.m
@@ -3,6 +3,8 @@ function all_tetrodes(path,channels,fs,fs_new,states,pt5,rat,sd,sleep_name,tetro
 %channels:- cell array consisting of channels of all tetrodes individual each array consisting of
 %channels of one tetrode
 %tetrodes:- array of characters of tetrode number
+
+%Iterate through tetrodes
 parfor i = 1:length(channels)
     one_tetrode(path,channels{i},fs,fs_new,states,pt5,rat,sd,sleep_name,tetrodes{i});
 end
