2d80e18c22ee3b6d4ce9b0c7a5b17a96ff9fcdcc
Adrián Alemán-Z <r.a.aleman.zapata@student.tue.nl>
2021-01-14 00:10:24 +0100

    adding comment all_sleep

--- Scripts Updated/all_sleep.m
+++ Scripts Updated/all_sleep.m
@@ -16,11 +16,15 @@ function all_sleep(path_main,state_path,fs,fs_new,rat,sd,channels,tetrodes)
 sleep = {{'Presleep','presleep'} , {'post_Trial1','post_trial1'} , {'post_Trial2','post_trial2'} , {'post_Trial3','post_trial3'} , {'post_Trial4','post_trial4'}} ;
 sleep_names = {'PRE','PT1','PT2','PT3','PT4'};
 
+%Iterate through trials.
 parfor a = 1:length(sleep)
+    %Fetch sleep scoring
     filename = getstates(state_path,sleep{a});
     states = load(strcat(state_path,filename),'states');
     states = states.states;
+    %Fetch trial folder name        
     fold = getfoldername(path_main,sleep{a});
+    %Run detection on all tetrodes            
     tic
     all_tetrodes(strcat(path_main,fold,'\'),channels,fs,fs_new,states,0,rat,sd,sleep_names{a},tetrodes);
     toc
