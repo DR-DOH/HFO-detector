9954775c7dc55e230e7d979704e0ff8a3ed42170
Adrián Alemán-Z <r.a.aleman.zapata@student.tue.nl>
2021-01-15 18:56:53 +0100

    Adding more Uppercase options

--- Scripts Updated/getfoldername.m
+++ Scripts Updated/getfoldername.m
@@ -5,6 +5,9 @@ A = A(~ismember({A.name},{'.','..'})); %Remove dots
 A = A([A.isdir]); %Only folders.
 A = {A.name};
 fold = A(or(cellfun(@(x) ~isempty(strfind(x,str{1})),A),cellfun(@(x) ~isempty(strfind(x,str{2})),A)));
+if isempty(fold)
+fold = A(or(cellfun(@(x) ~isempty(strfind(x,str{3})),A),cellfun(@(x) ~isempty(strfind(x,str{4})),A)));
+end
 fold = cell2mat(fold);
 
 end
