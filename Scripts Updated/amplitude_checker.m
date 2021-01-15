0c17eb5238036127539b79cdff9f9d9190012ee7
Adrián Alemán-Z <r.a.aleman.zapata@student.tue.nl>
2021-01-15 02:40:28 +0100

    rename

--- Scripts Updated/amplitude_checker.m
+++ Scripts Updated/amplitude_checker.m
@@ -1,10 +1,10 @@
-function val = amplitude_checker(signal,threshold,window,stride,threshold_ratio)
+function val = amplitude_checker(temp,threshold_amplitude,window,stride,threshold_ratio_amplitude)
 %To check whether the a certain percentage of peaks cross the threshold limit or not, outputting a binary
 %value
 val = 0;
 i = 1;
-while i <= length(signal) + 1 - window
-    if threshold_peak_ratio(signal(i:i+window-1),threshold) >= threshold_ratio
+while i <= length(temp) + 1 - window
+    if threshold_peak_ratio(temp(i:i+window-1),threshold_amplitude) >= threshold_ratio_amplitude
         val = 1;
         break;
     end
